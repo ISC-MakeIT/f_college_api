d87db1016ac862cdb7661ad8e1f3c3ec9bcc4c49
ymzk-jp <ymzk@gmail.com>
2018-08-31 19:32:23 +0900

    some

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-// ###### Sun Jul 29 15:11:51 JST 2018
-'use strict';
-const express = require('express');
-const app = express();
-const path = require('path');
-const mysql = require('mysql2');
-const PORT = process.env.PORT || 4000;
-const debug = require('debug')('app:server');
-const http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testPage = require('./routes/test')
 
-app.use('/', express.static(path.join(__dirname, 'views')));
-// app.set('public', __dirname + '/public');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-const server = app.listen(PORT, function () {
-    console.log('http://localhost:' + server.address().port);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/test', testPage);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
-app.get('/', (req, res, next) => {
-    res.send('hello node');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
