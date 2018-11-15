7a17740381e0d1917a9eaa86f92a7e0e3fbb194b
ymzk-jp <ymzk@gmail.com>
2018-11-15 10:28:35 +0900

    get product detail and photos and menbers

--- routes/products/products.js
+++ routes/products/products.js
@@ -12,11 +12,7 @@ const connection = mysql.createConnection({
 
 // https://fc-fb-live.com/api/products/
 router.get('/', (req, res) => {
-    let products = [];
-    let fashion = {};
-    let beauty = {};
-    let fashion_products = [];
-    let beauty_products = [];
+    let products = {};
     const query = 'SELECT ' +
         'products.product_id, ' +
         'products.genre, ' +
@@ -34,8 +30,10 @@ router.get('/', (req, res) => {
         'ORDER BY products.genre desc, products.entry_order ASC ';
 
     connection.query(query, (err, rows) => {
+        let fashion_products = [];
+        let beauty_products = [];  
+        let items = {};
         for (let item of rows) {
-            let items = {};
             items = {
                 product_id: item.product_id,
                 entry_order: item.entry_order,
@@ -55,10 +53,8 @@ router.get('/', (req, res) => {
                 fashion_products.push(items);
             }
         }
-        fashion.fashion = fashion_products;
-        beauty.beauty = beauty_products;
-        products.push(fashion);
-        products.push(beauty);
+        products.fashion = fashion_products;
+        products.beauty = beauty_products;
         res.header('Content-Type', 'application/json; charset=utf-8');
         res.json(products);
     });
@@ -66,97 +62,83 @@ router.get('/', (req, res) => {
 
 // https://fc-fb-live.com/api/products/1
 router.get('/:id', (req, res) => {
-    productDetail().then(
-        product => {
-            console.log("bbbbbbbbbbbbbbbbbb");
-            console.log(toString.call(product));
-            console.log();
-            res.set('Content-Type', 'application/json; charset=utf-8');
-            var a = JSON.stringify(product);
-            res.json(a);
+
+    let product = {};
+    const query_for_products = 'SELECT ' +
+        'products.product_id, ' +
+        'products.genre, ' +
+        'products.theme, ' +
+        'products.concept ' +
+        'FROM products ' +
+        'WHERE products.product_id = 1';
+
+    connection.query(query_for_products, (err, caption) => {
+        let captions = [];
+        let items = {};
+        for (let item of caption) {
+            items = {
+                product_id: item.product_id,
+                genre: item.genre,
+                theme: item.theme,
+                concept: item.concept
+            };
+            captions.push(items);
         }
-    );
-});
-
-const productDetail = () => {
-    return new Promise((resolve) => {
-        let product = [];
-        let data={};
-        product.data=data;
-        const query_for_products = 'SELECT ' +
+        product.caption = captions;
+        const query_for_photos = 'SELECT ' +
             'products.product_id, ' +
-            'products.genre, ' +
-            'products.theme, ' +
-            'products.concept ' +
-            'FROM products ' +
-            'WHERE products.product_id = 1';
+            'photos.photo_path ' +
+            'FROM ' +
+            'products ' +
+            'JOIN ' +
+            'photos ON products.product_id = photos.product_id ' +
+            'WHERE ' +
+            'products.product_id=1';
 
-        connection.query(query_for_products, (err, caption) => {
-            let captions=[];
-            let items = {};
-            for (let item of caption) {
+        connection.query(query_for_photos, (err, photo) => {
+            let photos = [];
+            for (let item of photo) {
+                let items = {};
                 items = {
-                    product_id: item.product_id,
-                    genre: item.genre,
-                    theme: item.theme,
-                    concept: item.concept
+                    photo_path: item.photo_path
                 };
-                captions.push(items);
+                photos.push(items);
             }
-            product.data=captions;
-            const query_for_photos = 'SELECT ' +
+            product.photos = photos;
+            const query_for_menbers = 'SELECT ' +
                 'products.product_id, ' +
-                'photos.photo_path ' +
+                'product_members.student_id, ' +
+                'students.name,students.class, ' +
+                'product_members.leader_flg ' +
                 'FROM ' +
                 'products ' +
                 'JOIN ' +
-                'photos ON products.product_id = photos.product_id ' +
+                'product_members ON products.product_id = product_members.product_id ' +
+                'JOIN ' +
+                'students ON product_members.student_id = students.student_id ' +
                 'WHERE ' +
                 'products.product_id=1';
 
-            connection.query(query_for_photos, (err, photo) => {
-                let photos = [];
-                for (let item of photo) {
+            connection.query(query_for_menbers, (err, menber) => {
+                let menbers = [];
+                for (let item of menber) {
                     let items = {};
                     items = {
-                        photo_path: item.photo_path
+                        student_id: item.student_id,
+                        student_name: item.name,
+                        student_class: item.class
                     };
-                    photos.push(items);
-                }
-                product.photos = photos;
-                const query_for_menbers = 'SELECT ' +
-                    'products.product_id, ' +
-                    'product_members.student_id, ' +
-                    'students.name,students.class ' +
-                    'FROM ' +
-                    'products ' +
-                    'JOIN ' +
-                    'product_members ON products.product_id = product_members.product_id ' +
-                    'JOIN ' +
-                    'students ON product_members.student_id = students.student_id ' +
-                    'WHERE ' +
-                    'products.product_id=1';
-
-                connection.query(query_for_menbers, (err, menber) => {
-                    console.log(menber);
-                    let menbers = [];
-                    for (let item of menber) {
-                        let items = {};
-                        items = {
-                            student_id: item.student_id,
-                            student_name: item.name,
-                            student_class: item.class
-                        };
-                        menbers.push(items);
+                    if (item.leader_flg === 1) {
+                        items.leader_flg = true;
                     }
-                    product.menbers = menbers;
-                    console.log("aaaaaaaaaaaaaaaaa");
-                    console.log(product);
-                    resolve(product);
-                });
+                    menbers.push(items);
+                }
+                product.menbers = menbers;
+                res.set('Content-Type', 'application/json; charset=utf-8');
+                res.json(product);
             });
         });
     });
-}
+});
 
 module.exports = router;
