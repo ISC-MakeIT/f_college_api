b9ddb9f2f4cc00766c7b8d739c95e8ee8392a0b0
ymzk-jp <ymzk@gmail.com>
2018-11-18 14:05:08 +0900

    fix duplicated sql results

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, './public')));
+
 
 const path = require("path");
 
