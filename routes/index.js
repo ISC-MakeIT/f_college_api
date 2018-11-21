92bddf86c479296876ef5342dcc01570ef2365e1
wh1tecat <whitequeen0@gmail.com>
2018-11-21 17:21:10 +0900

    fix page reload issue by route settings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require("path");
+
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.send('index.html');
+router.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname,"../", "public", "index.html"));
 });
 
 module.exports = router;
